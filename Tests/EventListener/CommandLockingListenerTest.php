81d07e986e0a6efa3e0426fd446a9092a4387ce1
Matthias Molitor <mmo@webfactory.de>
2016-05-14 20:18:56 +0200

    implemented test

--- Tests/EventListener/CommandLockingListenerTest.php
+++ Tests/EventListener/CommandLockingListenerTest.php
@@ -2,11 +2,39 @@
 
 namespace Matthimatiker\CommandLockingBundle\Tests\EventListener;
 
+use Matthimatiker\CommandLockingBundle\EventListener\CommandLockingListener;
+use Webfactory\Constraint\IsEventSubscriberConstraint;
+
 class CommandLockingListenerTest extends \PHPUnit_Framework_TestCase
 {
+    /**
+     * System under test.
+     *
+     * @var CommandLockingListener
+     */
+    private $listener = null;
+
+    /**
+     * Initializes the test environment.
+     */
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->listener = new CommandLockingListener('mock');
+    }
+
+    /**
+     * Cleans up the test environment.
+     */
+    protected function tearDown()
+    {
+        $this->listener = null;
+        parent::tearDown();
+    }
+
     public function testIsValidEventSubscriber()
     {
-
+        $this->assertThat($this->listener, new IsEventSubscriberConstraint());
     }
 
     public function testAddsLockOption()
