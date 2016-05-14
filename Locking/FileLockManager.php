f626bd3af19f3084c14a742efa42ca7e34e10c97
Matthias Molitor <mmo@webfactory.de>
2016-05-14 19:17:50 +0200

    implemented lock manager

--- Locking/FileLockManager.php
+++ Locking/FileLockManager.php
@@ -2,6 +2,8 @@
 
 namespace Matthimatiker\CommandLockingBundle\Locking;
 
+use Symfony\Component\Filesystem\LockHandler;
+
 /**
  * Uses files to create locks.
  *
@@ -10,11 +12,27 @@ namespace Matthimatiker\CommandLockingBundle\Locking;
 class FileLockManager implements LockManagerInterface
 {
     /**
+     * The directory that contains the lock files.
+     *
+     * @var string
+     */
+    private $lockDirectory = null;
+
+    /**
+     * Contains the locks that are currently active.
+     *
+     * The name of the lock is used as key.
+     *
+     * @var array<string, LockHandler>
+     */
+    private $activeLocksByName = array();
+
+    /**
      * @param string $lockDirectory Path to the directory that contains the locks.
      */
     public function __construct($lockDirectory)
     {
-
+        $this->lockDirectory = $lockDirectory;
     }
 
     /**
@@ -27,7 +45,16 @@ class FileLockManager implements LockManagerInterface
      */
     public function lock($name)
     {
-        // TODO: Implement lock() method.
+        if ($this->isLocked($name)) {
+            return false;
+        }
+        $lock = new LockHandler($name, $this->lockDirectory);
+        if ($lock->lock()) {
+            // Obtained lock.
+            $this->activeLocksByName[$name] = $lock;
+            return true;
+        }
+        return false;
     }
 
     /**
@@ -39,7 +66,23 @@ class FileLockManager implements LockManagerInterface
      */
     public function release($name)
     {
-        // TODO: Implement release() method.
+        if (!$this->isLocked($name)) {
+            return;
+        }
+        /* @var $lock LockHandler */
+        $lock = $this->activeLocksByName[$name];
+        $lock->release();
+        unset($this->activeLocksByName[$name]);
     }
 
+    /**
+     * Checks if a lock with the given name is active.
+     *
+     * @param string $name
+     * @return boolean
+     */
+    private function isLocked($name)
+    {
+        return isset($this->activeLocksByName[$name]);
+    }
 }
