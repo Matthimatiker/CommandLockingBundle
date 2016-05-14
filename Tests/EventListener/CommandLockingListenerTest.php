d4b47a2f0c717b72b1c622790368c7298f973ca4
Matthias Molitor <mmo@webfactory.de>
2016-05-14 19:57:28 +0200

    added test stubs

--- Tests/EventListener/CommandLockingListenerTest.php
+++ Tests/EventListener/CommandLockingListenerTest.php
@@ -4,5 +4,48 @@ namespace Matthimatiker\CommandLockingBundle\Tests\EventListener;
 
 class CommandLockingListenerTest extends \PHPUnit_Framework_TestCase
 {
+    public function testAddsLockOption()
+    {
 
+    }
+
+    public function testDoesNotLockIfNotRequested()
+    {
+
+    }
+
+    public function testLocksIfRequested()
+    {
+
+    }
+
+    public function testRunsCommandIfNoLockIsRequested()
+    {
+
+    }
+
+    public function testDoesNotRunCommandIfLockingFails()
+    {
+
+    }
+
+    public function testRunsCommandIfLockingSucceeds()
+    {
+
+    }
+
+    public function testReleasesLockWhenLockedCommandTerminates()
+    {
+
+    }
+
+    public function testDoesNotReleaseWhenCommandWasNotLocked()
+    {
+
+    }
+
+    public function testDoesNotInterfereWithOptionsOfCommand()
+    {
+
+    }
 }
