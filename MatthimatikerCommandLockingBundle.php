dd8035b24e2cd393dbc58f3576a254649f7c705b
Matthias Molitor <mmo@webfactory.de>
2016-05-16 23:31:01 +0200

    register the compiler pass

--- MatthimatikerCommandLockingBundle.php
+++ MatthimatikerCommandLockingBundle.php
@@ -2,8 +2,25 @@
 
 namespace Matthimatiker\CommandLockingBundle;
 
+use Matthimatiker\CommandLockingBundle\DependencyInjection\Compiler\RegisterLockManagersPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class MatthimatikerCommandLockingBundle extends Bundle
 {
+    /**
+     * Builds the bundle.
+     *
+     * It is only ever called once when the cache is empty.
+     *
+     * This method can be overridden to register compilation passes,
+     * other extensions, ...
+     *
+     * @param ContainerBuilder $container A ContainerBuilder instance
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+        $container->addCompilerPass(new RegisterLockManagersPass());
+    }
 }
