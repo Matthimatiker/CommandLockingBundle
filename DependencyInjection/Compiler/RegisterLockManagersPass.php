90888ddecd4f62249de72dab464d9f3b9c320219
Matthias Molitor <mmo@webfactory.de>
2016-05-16 23:29:46 +0200

    implemented compiler pass

--- DependencyInjection/Compiler/RegisterLockManagersPass.php
+++ DependencyInjection/Compiler/RegisterLockManagersPass.php
@@ -4,6 +4,8 @@ namespace Matthimatiker\CommandLockingBundle\DependencyInjection\Compiler;
 
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Exception\InvalidArgumentException;
+use Symfony\Component\DependencyInjection\Reference;
 
 /**
  * Registers all tagged LockManagers.
@@ -25,6 +27,19 @@ class RegisterLockManagersPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        // TODO: Implement process() method.
+        $listener = $container->findDefinition(self::LOCKING_LISTENER_SERVICE_ID);
+        foreach ($container->findTaggedServiceIds(self::LOCK_MANAGER_TAG) as $serviceId => $tags) {
+            /* @var $serviceId string */
+            /* @var $tags array<string, string>[] */
+            foreach ($tags as $tag) {
+                /* @var $tag array<string, string> */
+                if (!isset($tag['alias'])) {
+                    $message = '%s tag for service %s must also contain an alias attribute that '
+                             . 'is used to reference the lock manager.';
+                    throw new InvalidArgumentException(sprintf($message, self::LOCK_MANAGER_TAG, $serviceId));
+                }
+                $listener->addMethodCall('registerLockManager', array($tag['alias'], new Reference($serviceId)));
+            }
+        }
     }
 }
