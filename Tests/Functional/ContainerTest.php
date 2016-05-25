589bb238ff0a8c158c306b82723f60fc9366af90
Matthias Molitor <mmo@webfactory.de>
2016-05-25 09:00:56 +0200

    added test case stub (#1)

---
+++ Tests/Functional/ContainerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Matthimatiker\CommandLockingBundle\Tests\Functional;
+
+/**
+ * Tests the service configuration.
+ */
+class ContainerTest
+{
+    public function testHasListener()
+    {
+
+    }
+
+    public function testDefaultLockManagerIsAvailable()
+    {
+
+    }
+}
